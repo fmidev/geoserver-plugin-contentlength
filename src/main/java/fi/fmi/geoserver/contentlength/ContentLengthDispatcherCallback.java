b8d1be4bc49819361dc33e8c899b75260cf685e3
Ville Karppinen <ville.karppinen@fmi.fi>
2018-08-30 17:09:18 +0300

    Built for Geoserver 2.13.2

--- src/main/java/fi/fmi/geoserver/contentlength/ContentLengthDispatcherCallback.java
+++ src/main/java/fi/fmi/geoserver/contentlength/ContentLengthDispatcherCallback.java
@@ -1,54 +1,43 @@
 package fi.fmi.geoserver.contentlength;
 
-import org.geoserver.platform.Service;
-import org.geoserver.platform.ServiceException;
-import org.geoserver.platform.Operation;
+import org.geoserver.ows.DispatcherCallback;
 import org.geoserver.ows.Request;
 import org.geoserver.ows.Response;
-import org.geoserver.ows.DispatcherCallback;
+import org.geoserver.platform.Operation;
+import org.geoserver.platform.Service;
+import org.geoserver.platform.ServiceException;
 
 /**
  * Forces Content-Length into headers given by {org.geoserver.ows.Response}.
- * 
- * DispatcherCallback extensions are automatically loaded by GeoServer framework.
- * Framework {org.geoserver.ows.Dispatcher} instance calls {responseDispatched}
- * callback function of {org.geoserver.ows.DispatcherCallback} instances before
- * flushing response output stream. Therefore, callback may add additional header
- * into response before flush is done.
- * 
- * This class uses {ContentLengthResponse} to get the content length from the
- * stream content.
+ *
+ * <p>DispatcherCallback extensions are automatically loaded by GeoServer framework. Framework
+ * {org.geoserver.ows.Dispatcher} instance calls {responseDispatched} callback function of
+ * {org.geoserver.ows.DispatcherCallback} instances before flushing response output stream.
+ * Therefore, callback may add additional header into response before flush is done.
+ *
+ * <p>This class uses {ContentLengthResponse} to get the content length from the stream content.
  */
 public class ContentLengthDispatcherCallback implements DispatcherCallback {
 
-    /**
-     * See parent {org.geoserver.ows.DispatcherCallback} class for function description.
-     */
+    /** See parent {org.geoserver.ows.DispatcherCallback} class for function description. */
     public Request init(Request request) {
         // Nothing to do.
         return null;
     }
 
-    /**
-     * See parent {org.geoserver.ows.DispatcherCallback} class for function description.
-     */
-    public Service serviceDispatched(Request request, Service service)
-            throws ServiceException {
+    /** See parent {org.geoserver.ows.DispatcherCallback} class for function description. */
+    public Service serviceDispatched(Request request, Service service) throws ServiceException {
         // Nothing to do.
         return null;
     }
 
-    /**
-     * See parent {org.geoserver.ows.DispatcherCallback} class for function description.
-     */
+    /** See parent {org.geoserver.ows.DispatcherCallback} class for function description. */
     public Operation operationDispatched(Request request, Operation operation) {
         // Nothing to do.
         return null;
     }
 
-    /**
-     * See parent {org.geoserver.ows.DispatcherCallback} class for function description.
-     */
+    /** See parent {org.geoserver.ows.DispatcherCallback} class for function description. */
     public Object operationExecuted(Request request, Operation operation, Object result) {
         // Nothing to do.
         return null;
@@ -56,19 +45,15 @@ public class ContentLengthDispatcherCallback implements DispatcherCallback {
 
     /**
      * Called after the response to a request has been dispatched.
-     * <p>
-     * Wraps the given {response} if Content-Length should be included
-     * as part of the {response} headers.
-     * </p>
-     * <p>
-     * <b>Note:</b> This method is only called when the operation returns a
-     * value.
-     * </p>
-     * <p>
-     * This method can modify the response object, or wrap and return it. If
-     * null is returned the response passed in is used normally.
-     * </p>
-     * 
+     *
+     * <p>Wraps the given {response} if Content-Length should be included as part of the {response}
+     * headers.
+     *
+     * <p><b>Note:</b> This method is only called when the operation returns a value.
+     *
+     * <p>This method can modify the response object, or wrap and return it. If null is returned the
+     * response passed in is used normally.
+     *
      * @param request The request.
      * @param operation The operation.
      * @param result The result of the operation.
@@ -78,11 +63,9 @@ public class ContentLengthDispatcherCallback implements DispatcherCallback {
             Request request, Operation operation, Object result, Response response) {
         Response wrapperResponse = null;
         try {
-            ContentLengthResponse contentLengthResponse = 
-                    new ContentLengthResponse(response);
+            ContentLengthResponse contentLengthResponse = new ContentLengthResponse(response);
             // Set the content length header.
-            if (contentLengthResponse.
-                    setContentLength(request, operation, result, response)) {
+            if (contentLengthResponse.setContentLength(request, operation, result, response)) {
                 // Use the wrapper response in the framework flow
                 // because Content-Length header was set.
                 wrapperResponse = contentLengthResponse;
@@ -95,11 +78,8 @@ public class ContentLengthDispatcherCallback implements DispatcherCallback {
         return wrapperResponse;
     }
 
-    /**
-     * See parent {org.geoserver.ows.DispatcherCallback} class for function description.
-     */
+    /** See parent {org.geoserver.ows.DispatcherCallback} class for function description. */
     public void finished(Request request) {
         // Nothing to do.
     }
-
 }
--- src/main/java/fi/fmi/geoserver/contentlength/ContentLengthResponse.java
+++ src/main/java/fi/fmi/geoserver/contentlength/ContentLengthResponse.java
@@ -1,12 +1,9 @@
 package fi.fmi.geoserver.contentlength;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.Collections;
-import java.util.Set;
 import javax.servlet.http.HttpServletResponse;
-
 import org.geoserver.ows.Request;
 import org.geoserver.ows.Response;
 import org.geoserver.platform.Operation;
@@ -14,16 +11,15 @@ import org.geoserver.platform.ServiceException;
 
 /**
  * Forces Content-Length header for the response if necessary.
- * 
- * {ContentLengthResponse} class is meant to be created by its own package classes
- * and it is provided as {org.geoserver.ows.Response} class for other framework classes.
- * This {org.geoserver.ows.Response} class wrapper is used by {ContentLengthDispatcherCallback}
- * class.
+ *
+ * <p>{ContentLengthResponse} class is meant to be created by its own package classes and it is
+ * provided as {org.geoserver.ows.Response} class for other framework classes. This
+ * {org.geoserver.ows.Response} class wrapper is used by {ContentLengthDispatcherCallback} class.
  */
 class ContentLengthResponse extends Response {
 
     /** Header key for Content-Length. */
-    private final static String CONTENT_LENGTH_HEADER = "Content-Length";
+    private static final String CONTENT_LENGTH_HEADER = "Content-Length";
 
     /** Response that this class wraps. */
     private Response response;
@@ -31,24 +27,19 @@ class ContentLengthResponse extends Response {
     /** Output stream is temporarily kept as byte content. */
     private byte[] content;
     /**
-     * Content-Length of the output stream.
-     * Negative value means that the length has not been set.
+     * Content-Length of the output stream. Negative value means that the length has not been set.
      */
     private int contentLength = -1;
 
     /**
      * Set the output stream content into the byte array.
-     * 
-     * @param {Operation} operation The operation for the request.
-     *                              May be {null}.
-     * @param {Object} result Operation result object whose content size is measured.
-     *                        May be {null}.
-     * @param {Response} response The response to the operation. 
-     *                            May be {null}.
+     *
+     * @param {Operation} operation The operation for the request. May be {null}.
+     * @param {Object} result Operation result object whose content size is measured. May be {null}.
+     * @param {Response} response The response to the operation. May be {null}.
      * @throws {IOException} Any I/O errors that occur.
      */
-    private void setContent(
-            final Operation operation, final Object result, final Response response)
+    private void setContent(final Operation operation, final Object result, final Response response)
             throws IOException {
         // Set the content if it has not already been set.
         if (null == content) {
@@ -76,9 +67,8 @@ class ContentLengthResponse extends Response {
 
     /**
      * Constructor to wrap response.
-     * 
-     * @param {Response} The response object that should be wrapped.
-     *                   May not be {null}.
+     *
+     * @param {Response} The response object that should be wrapped. May not be {null}.
      */
     public ContentLengthResponse(Response response) {
         // Original response getters are used to properly initialize
@@ -89,17 +79,13 @@ class ContentLengthResponse extends Response {
 
     /**
      * Set the content length if it does not exist in the {HttpServletResponse}.
-     * <p>
-     * This function gets {result} content size which will be set
-     * for the Content-Length header when framework sets headers
-     * for the response.
-     * </p>
-     * <p>
-     * Notice, stream needs to be read into byte array if this function
-     * is used. Then, excess memory needs to be used to temporarily save
-     * stream content.
-     * </p>
-     * 
+     *
+     * <p>This function gets {result} content size which will be set for the Content-Length header
+     * when framework sets headers for the response.
+     *
+     * <p>Notice, stream needs to be read into byte array if this function is used. Then, excess
+     * memory needs to be used to temporarily save stream content.
+     *
      * @param request The request.
      * @param operation The operation.
      * @param result The result of the operation.
@@ -113,11 +99,13 @@ class ContentLengthResponse extends Response {
         boolean contentLengthSet = false;
         // Content-Length header is forced into HttpServletResponse
         // if the header does not already exist.
-        if (null != request && null != operation && null != result
-                && null != response && response.canHandle(operation)) {
+        if (null != request
+                && null != operation
+                && null != result
+                && null != response
+                && response.canHandle(operation)) {
             final HttpServletResponse httpResponse = request.getHttpResponse();
-            if (null != httpResponse
-                    && !httpResponse.containsHeader(CONTENT_LENGTH_HEADER)) {
+            if (null != httpResponse && !httpResponse.containsHeader(CONTENT_LENGTH_HEADER)) {
                 // Framework uses the result object for the outputstream content.
                 // Handle stream content to get the content length for the response.
                 setContent(operation, result, response);
@@ -126,25 +114,21 @@ class ContentLengthResponse extends Response {
         }
         return contentLengthSet;
     }
-    
+
     /**
      * Serializes <code>value</code> to <code>output</code>.
-     * <p>
-     * The function uses the byte array {content} if it is available.
-     * Also, if {content} is used here, reference is set to {null} and
-     * the content is not available after that. The reason for this
-     * is to relese the content that is originally gotten from the
-     * stream to get the content length. Because content providers may
-     * provide the content stream only once, the content needs to be
-     * hold temporarily until it is asked once more by the framework.
-     * After that, original functionality of the framework may be used.
-     * Then, framework flow acts as the wrapped {org.geoserver.ows.Response}
-     * would have been used without the content length operation in the
+     *
+     * <p>The function uses the byte array {content} if it is available. Also, if {content} is used
+     * here, reference is set to {null} and the content is not available after that. The reason for
+     * this is to relese the content that is originally gotten from the stream to get the content
+     * length. Because content providers may provide the content stream only once, the content needs
+     * to be hold temporarily until it is asked once more by the framework. After that, original
+     * functionality of the framework may be used. Then, framework flow acts as the wrapped
+     * {org.geoserver.ows.Response} would have been used without the content length operation in the
      * middle of the flow.
-     * </p>
-     * <p>
-     * The <code>operation</code> bean is provided for context.
-     * </p>
+     *
+     * <p>The <code>operation</code> bean is provided for context.
+     *
      * @param value The value to serialize.
      * @param output The output stream.
      * @param operation The operation which resulted in <code>value</code>
@@ -164,43 +148,34 @@ class ContentLengthResponse extends Response {
         }
     }
 
-    /**
-     * See parent {org.geoserver.ows.Response} class for function description.
-     */
+    /** See parent {org.geoserver.ows.Response} class for function description. */
     public boolean canHandle(Operation operation) {
         return response.canHandle(operation);
     }
 
-    /**
-     * See parent {org.geoserver.ows.Response} class for function description.
-     */
-    public String getMimeType(Object value, Operation operation)
-            throws ServiceException {
+    /** See parent {org.geoserver.ows.Response} class for function description. */
+    public String getMimeType(Object value, Operation operation) throws ServiceException {
         return response.getMimeType(value, operation);
     }
 
     /**
-     * Returns a 2xn array of Strings, each of which is an HTTP header pair
-     * to be set on the HTTP Response.  Can return null if there are
-     * no headers to be set on the response.
-     * <p>
-     * If content header has been set, this function will append Content-Length
-     * header to the 2D header array that is provided by the {Response} class
-     * instance that this class instance wraps.
-     * </p>
-     * 
+     * Returns a 2xn array of Strings, each of which is an HTTP header pair to be set on the HTTP
+     * Response. Can return null if there are no headers to be set on the response.
+     *
+     * <p>If content header has been set, this function will append Content-Length header to the 2D
+     * header array that is provided by the {Response} class instance that this class instance
+     * wraps.
+     *
      * @param value The value to serialize
      * @param operation The operation being performed.
-     *
      * @return 2xn string array containing string-pairs of HTTP headers/values
      */
-    public String[][] getHeaders(Object value, Operation operation)
-            throws ServiceException {
+    public String[][] getHeaders(Object value, Operation operation) throws ServiceException {
         // Get headers from the wrapped response.
         String[][] headers = response.getHeaders(value, operation);
         if (contentLength >= 0) {
             // Content length header needs to be added.
-            if(null == headers) {
+            if (null == headers) {
                 // Only Content-Length header will be in the response headers.
                 // Create the 2D array for the content.
                 headers = new String[1][2];
@@ -227,7 +202,7 @@ class ContentLengthResponse extends Response {
                     // Set also the header value.
                     newHeaders[i][1] = headers[i][1];
                 }
-                if(!alreadyExists) {
+                if (!alreadyExists) {
                     // Append Content-Length header because it
                     // did not exist in the original headers.
                     newHeaders[originalCount][0] = CONTENT_LENGTH_HEADER;
@@ -240,16 +215,12 @@ class ContentLengthResponse extends Response {
         return headers;
     }
 
-    /**
-     * See parent {org.geoserver.ows.Response} class for function description.
-     */
+    /** See parent {org.geoserver.ows.Response} class for function description. */
     public String getPreferredDisposition(Object value, Operation operation) {
         return response.getPreferredDisposition(value, operation);
     }
 
-    /**
-     * See parent {org.geoserver.ows.Response} class for function description.
-     */
+    /** See parent {org.geoserver.ows.Response} class for function description. */
     public String getAttachmentFileName(Object value, Operation operation) {
         return response.getAttachmentFileName(value, operation);
     }
